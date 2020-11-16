3f96172036f1247a01c1e6d3a7f4a0161fc46ee7
EC2 Default User <ec2-user@ip-172-31-79-54.ec2.internal>
2020-11-16 16:15:14 +0000

    ルーティングの設定と、customer/item/orderのモデル・ビュー・コントローラの追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,28 @@ Rails.application.routes.draw do
   devise_for :admins
   namespace :admin do
     # resourses :
+    resources :customers,only: [:index, :show, :edit, :update]
+    resources :orders,only: [:index, :show, :update]
   end
 
   # customer
   devise_for :customers
   scope module: :customer do
-    # resourses :
+
+    get 'about' => 'customer/items#about'
+    root to: 'customer/items#top'
+
+
+
+    resources :customers,only: [:show, :edit, :update] do
+      collection do
+        get 'quit'
+  	    patch 'out'
+      end
+    end
+
+    resources :items,only: [:about, :index,:show]
+
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
