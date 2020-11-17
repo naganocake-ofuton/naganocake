2aba9d80f945820c4cfa43358830aac91076e35a
EC2 Default User <ec2-user@ip-172-31-79-54.ec2.internal>
2020-11-17 12:00:26 +0000

    shippingコントローラ・ビューの作成

--- db/schema.rb
+++ db/schema.rb
@@ -35,11 +35,19 @@ ActiveRecord::Schema.define(version: 2020_11_17_093224) do
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "last_name"
+    t.string "first_name"
+    t.string "last_name_kana"
+    t.string "first_name_kana"
+    t.string "telephone_number"
+    t.string "postcode"
+    t.string "address"
+    t.boolean "is_deleted"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
@@ -47,17 +55,6 @@ ActiveRecord::Schema.define(version: 2020_11_17_093224) do
   create_table "genres", force: :cascade do |t|
     t.string "name"
     t.boolean "is_active", default: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "items", force: :cascade do |t|
-    t.integer "genre_id"
-    t.string "name"
-    t.integer "price"
-    t.text "body"
-    t.boolean "is_active"
-    t.string "image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -73,6 +70,14 @@ ActiveRecord::Schema.define(version: 2020_11_17_093224) do
   end
 
   create_table "orders", force: :cascade do |t|
+    t.integer "customer_id"
+    t.integer "postage", default: 800
+    t.integer "pay_amount"
+    t.integer "payment_method", default: 0
+    t.string "name"
+    t.string "postcode"
+    t.string "address"
+    t.integer "status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
