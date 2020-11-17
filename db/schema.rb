97045a6ec3875bdd1a76326919319a5aa026fcec
EC2 Default User <ec2-user@ip-172-31-46-218.ec2.internal>
2020-11-17 04:33:22 +0000

    [add]ユーザーの新規登録画面表示と日本語化、ユーザーログイン画面表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
 ActiveRecord::Schema.define(version: 2020_11_16_161024) do
 
   create_table "admins", force: :cascade do |t|
@@ -32,36 +30,35 @@ ActiveRecord::Schema.define(version: 2020_11_16_161024) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "last_name"
     t.string "first_name"
-    t.string "last_name_kana"
-    t.string "first_name_kana"
-    t.string "telephone_number"
-    t.string "postcode"
-    t.string "address"
-   
-    t.boolean "is_deleted", default: false, null: false
+    t.string "last_name"
+    t.string "kana_first_name"
+    t.string "kana_last_name"
+    t.string "postal_code"
+    t.string "residence"
+    t.string "phone_number"
+    t.boolean "is_valid", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
-
-  create_table "shippings", force: :cascade do |t|
-    t.integer "customer_id"
-    t.string "name"
-    t.string "postcode"
-    t.string "address"
-  end
-  
   create_table "items", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "orders", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "shippings", force: :cascade do |t|
+    t.integer "customer_id"
+    t.string "name"
+    t.string "postcode"
+    t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
