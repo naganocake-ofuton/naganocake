2ac496724cd65f004eb117460394447a2e8a955a
otchi012 <hiroshi012931@gmail.com>
2020-11-17 13:02:50 +0000

    [Add]カートのコントローラー追加

--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -2,5 +2,5 @@ class Shipping < ApplicationRecord
   belongs_to :customer
 
   validates :customer_id, :name, :address, presence: true
-	validates :postcode, length: {is: 7}
+  validates :postcode, length: {is: 7}
 end
--- db/migrate/20201117073108_users.rb
+++ db/migrate/20201117073108_users.rb
@@ -1,5 +1,5 @@
 class Users < ActiveRecord::Migration[5.2]
   def change
-    drop_table :items
+    drop_table :users
   end
 end
