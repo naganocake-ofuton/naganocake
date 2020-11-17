2ac0ccff15cb8d20a6a8333bc23123e0ab265801
otchi012 <hiroshi012931@gmail.com>
2020-11-17 10:40:21 +0000

    [Add]全モデルの追加、不要なitemsのマイグレファイルの削除

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,8 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema.define(version: 2020_11_16_161024) do
+
+ActiveRecord::Schema.define(version: 2020_11_17_093224) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,25 +24,31 @@ ActiveRecord::Schema.define(version: 2020_11_16_161024) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "item_id"
+    t.integer "customer_id"
+    t.integer "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "last_name"
-    t.string "first_name"
-    t.string "last_name_kana"
-    t.string "first_name_kana"
-    t.string "telephone_number"
-    t.string "postcode"
-    t.string "address"
-    t.boolean "is_deleted"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.boolean "is_active", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "items", force: :cascade do |t|
@@ -55,15 +62,17 @@ ActiveRecord::Schema.define(version: 2020_11_16_161024) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "order_items", force: :cascade do |t|
+    t.integer "item_id"
+    t.integer "order_id"
+    t.integer "amount"
+    t.integer "making_status", default: 0
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "orders", force: :cascade do |t|
-    t.integer "customer_id"
-    t.integer "postage", default: 800
-    t.integer "pay_amount"
-    t.integer "payment_method", default: 0
-    t.string "name"
-    t.string "postcode"
-    t.string "address"
-    t.integer "status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -76,6 +85,5 @@ ActiveRecord::Schema.define(version: 2020_11_16_161024) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 
 end
