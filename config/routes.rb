5aeded569018edc76d3b6b361cfc866230b4ebb9
EC2 Default User <ec2-user@ip-172-31-79-54.ec2.internal>
2020-11-17 03:08:02 +0000

    ルーティングの更新

--- config/routes.rb
+++ config/routes.rb
@@ -7,25 +7,58 @@ Rails.application.routes.draw do
     resources :orders,only: [:index, :show, :update]
   end
 
+
+
+
   # customer
-  devise_for :customers
-  scope module: :customer do
+   devise_for :customers, :controllers => {
+    :sessions => 'customers/sessions',
+    :registrations => 'customers/registrations',
+    :passwords => 'customers/passwords'
+   }
 
-    get 'about' => 'customer/items#about'
-    root to: 'customer/items#top'
+   get 'about' => 'customer/items#about'
+   root 'customer/items#top'
 
 
+   scope module: :customer do
+    resources :items,only: [:index,:show]
 
-    resources :customers,only: [:show, :edit, :update] do
-      collection do
-        get 'quit'
-  	    patch 'out'
+  	resource :customers,only: [:show, :edit, :update] do
+  		collection do
+  	     get 'quit'
+  	     patch 'out'
+  	  end
+
+      resources :cart_items,only: [:index,:update,:create,:destroy] do
+        collection do
+          delete '/' => 'cart_items#all_destroy'
+        end
       end
+
+      resources :orders,only: [:new,:index,:show,:create] do
+        collection do
+          post 'confirm'
+          get 'conmplete'
+        end
+      end
+
+      resources :shippings,only: [:index,:create,:edit,:update,:destroy]
     end
-
-    resources :items,only: [:about, :index,:show]
-
   end
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
