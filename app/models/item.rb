6cee8fae6fddbfa0716889faca308ae13bf37136
EC2 Default User <ec2-user@ip-172-31-79-54.ec2.internal>
2020-11-17 12:28:34 +0000

    itemコントローラ・ビューの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,12 @@
 class Item < ApplicationRecord
     belongs_to :genre
 	has_many :cart_items
-	has_many :orders, through: :order_details
+	has_many :orders, through: :order_items
 	has_many :order_items
 
 	attachment :image
 
-	validates :genre_id, :name, :tax_out_price, presence: true
+	validates :genre_id, :name, :price, presence: true
 	validates :explanation, length: {maximum: 200}
-	validates :tax_out_price, numericality: { only_integer: true }
+	validates :price, numericality: { only_integer: true }
 end
