09af008a43b061314ee9bf6d105ed3cfbfbaa332
EC2 Default User <ec2-user@ip-172-31-46-218.ec2.internal>
2020-11-19 02:54:57 +0000

    test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_18_025000) do
+ActiveRecord::Schema.define(version: 2020_11_17_093224) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -35,12 +35,6 @@ ActiveRecord::Schema.define(version: 2020_11_18_025000) do
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "last_name"
     t.string "first_name"
     t.string "last_name_kana"
@@ -49,7 +43,11 @@ ActiveRecord::Schema.define(version: 2020_11_18_025000) do
     t.string "postcode"
     t.string "address"
     t.boolean "is_deleted", default: false, null: false
-
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
@@ -57,6 +55,17 @@ ActiveRecord::Schema.define(version: 2020_11_18_025000) do
   create_table "genres", force: :cascade do |t|
     t.string "name"
     t.boolean "is_active", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.integer "genre_id"
+    t.string "name"
+    t.integer "price"
+    t.text "body"
+    t.boolean "is_active"
+    t.string "image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
